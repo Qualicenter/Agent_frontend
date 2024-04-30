5998815c9db915ab6fe94a776db7d7a4599591c0
Ivan <a01747131@itesm.mx>
2024-04-30 15:00:50 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import TitleComponent from "./components/Title";
+
+const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 40% 60%;
+  grid-gap: 10px;
+  height: 100vh;
+`;
+
+const LeftColumn = styled.div`
+  display: grid;
+  grid-template-rows: 50% 50%;
+  justify-items: center;
+`;
+
+const RightColumn = styled.div`
+  display: grid;
+  grid-template-rows: 33.33% 33.33% 33.33%;
+  justify-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <LeftColumn>
+        <TitleComponent text="Acciones Rápidas" />
+        <TitleComponent text="Transcripción" />
+      </LeftColumn>
+      <RightColumn>
+        <TitleComponent text="Métricas Llamadas" />
+        <TitleComponent text="Información del cliente" />
+        <TitleComponent text="Botones llamada" />
+      </RightColumn>
+    </Wrapper>
   );
 }
 
