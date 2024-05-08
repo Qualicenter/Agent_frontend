31e7f7bfce810fde54fa363856063496dec42fe7
Giozzu <A01754739@tec.mx>
2024-05-08 00:54:36 -0600

    Integración Contact Lens

--- src/components/Transcripcion.js
+++ src/components/Transcripcion.js
@@ -1,7 +1,28 @@
 import '../styles/transcripcion.css'
+import { ConnectContactLensClient, ListRealtimeContactAnalysisSegmentsCommand } from "@aws-sdk/client-connect-contact-lens"; // ES Modules import
+import { AWS_ACCESS_KEY_ID, AWS_REGION } from '../config';
+
+const config = {
+    region: AWS_REGION,
+    credentials: {
+      accessKeyId: AWS_ACCESS_KEY_ID // Reemplaza con tu Access Key ID // Reemplaza con tu Secret Access Key
+    },
+  };
+
+const client = new ConnectContactLensClient(config);
+
+const input = { // ListRealtimeContactAnalysisSegmentsRequest
+    InstanceId: "e730139b-8673-445e-8307-c3a9250199a2", // required
+    ContactId: "b89a2cbe-8ee5-498d-8fa9-9544563e9e72", // required
+};
+
+const command = new ListRealtimeContactAnalysisSegmentsCommand(input);
+const response = await client.send(command);
+console.log(response); // successful response
 
 const Transcripcion = () => {
     return(
+        
         <h1 className="ventana-transcripcion"> ESTO ES UNA PRUEBA</h1>
     )
     
---
+++ src/config/index.js
@@ -0,0 +1,2 @@
+exports.AWS_REGION = process.env.AWS_REGION || 'us-east-1';
+exports.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || '';
