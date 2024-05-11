9cfb676ce68b77c758d83b9fdbc6b13c7dfafe83
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-05-10 23:43:31 -0600

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,87 +1,37 @@
-import styled from "styled-components";
-import InfoCliente from "./components/InfoCliente";
-import Vehiculos from "./components/Vehiculos";
-import ClientScript from "./components/ClientScript";
-import { useState } from "react";
-import VentanaAyuda from "./components/VentanaAyuda";
-import ConnectStreamsComponent from './components/ConnectStreamsComponent';
-
-const Wrapper = styled.main`
-    position: relative;
-    width: 1194px;
-    height: 834px;
-    display: flex;
-    z-index: 0;
-`
-
-const Left = styled.section`
-    width: 30%;
-    height: 100%;
-    background-color: #f0f0f0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
-const Right = styled.section`
-    width: 70%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-
-    .arriba {
-        height: 30%;
-        display: flex;
-        padding: 10px;
-        gap: 20px
-    }
-
-    .abajo {
-        height: 70%;
-        padding: 10px;
-    }
-`
-const BotonAyuda = styled.button`
-    position: absolute;
-    right: 0;
-    top: 30%;
-    margin: 20px;
-    background: #872a7b;
-    color:  white;
-    font-size: 18px;
-    padding: 10px;
-    font-weight: 600;
-    border: none;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    cursor: pointer;
-`
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import LoginPage from "./LoginPage";
 
 function App() {
-    const [showVentanaAyuda, setShowVentanaAyuda] = useState(false);
+  const hasAccessToken = () => {
+    const token = sessionStorage.getItem("accessToken");
+    return !!token;
+  };
 
-    const showVentanaHandler  = () => {
-        setShowVentanaAyuda(!showVentanaAyuda)
-    }
-
-    return (
-        <Wrapper>
-            {showVentanaAyuda && <VentanaAyuda cancelar={showVentanaHandler}/>}
-            <Left>
-                <ConnectStreamsComponent/>
-                <BotonAyuda onClick={showVentanaHandler}>Solicitar Ayuda</BotonAyuda>
-            </Left>
-            <Right>
-                <div className="arriba">
-                    <InfoCliente nombre="Juan Perez" edad="25 años" poliza="Platino Ultra" tiempoEspera="--:--"/>
-                    <Vehiculos />
-                </div>
-                <div className="abajo">
-                    <ClientScript nombre="Juan Perez"/>
-                </div>
-            </Right>
-        </Wrapper>
-    );  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            hasAccessToken() ? (
+              <Navigate replace to="/home" />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
+        <Route path="/home" element={
+            hasAccessToken() ? (
+                <HomePage/>
+              ) : (
+                <Navigate replace to="/login" />
+              )
+        } />
+        <Route path="/login" element={<LoginPage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
