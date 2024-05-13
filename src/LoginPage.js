10903106e5dae8ee1585ddb01f85a9e8c8e47a84
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-05-12 22:07:24 -0600

    Agregados estilos a la loginpage

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,66 +1,105 @@
 import { useEffect, useState } from "react";
-import { signIn, signUp } from "./components/AuthService";
+import { signIn } from "./components/AuthService";
+import "./styles/login-page.css";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlinePassword } from "react-icons/md";
+import qualitas_logo from "./images/qualitas-logo.png";
 
 const LoginPage = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const usernameHandler = (e) => {
-        setUsername(e.target.value)
-    }
-    const passwordHandler = (e) => {
-        setPassword(e.target.value)
-    }
+  const usernameHandler = (e) => {
+    setUsername(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
 
-    useEffect(() => {
-        console.log("Username:", username);
-        console.log("Password:", password);
-    }, [username, password]);
+  useEffect(() => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+  }, [username, password]);
 
-    const loginHandler = async (e) => {
-        e.preventDefault();
-        try {
-          const session = await signIn(username, password);
-          console.log('Sign in successful', session);
-          if (session && typeof session.AccessToken !== 'undefined') {
-            sessionStorage.setItem('accessToken', session.AccessToken);
-            if (sessionStorage.getItem('accessToken')) {
-              window.location.href = '/home';
-            } else {
-              console.error('Session token was not set properly.');
-            }
-          } else {
-            console.error('SignIn session or AccessToken is undefined.');
-          }
-        } catch (error) {
-          alert(`Sign in failed: ${error}`);
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const session = await signIn(username, password);
+      console.log("Sign in successful", session);
+      if (session && typeof session.AccessToken !== "undefined") {
+        sessionStorage.setItem("accessToken", session.AccessToken);
+        if (sessionStorage.getItem("accessToken")) {
+          window.location.href = "/home";
+        } else {
+          console.error("Session token was not set properly.");
         }
-      };
+      } else {
+        console.error("SignIn session or AccessToken is undefined.");
+      }
+    } catch (error) {
+      alert(`Sign in failed: ${error}`);
+    }
+  };
 
-      const SignUpHandler = async (e) => {
-        e.preventDefault();
-        try {
-            await signUp(username, password);
-            alert('Sign up successful');
-          } catch (error) {
-            alert(`Sign up failed: ${error}`);
-          }
-      };
-
-    return (
-        <div>
-            <h1>LoginPage</h1>
-            <form onSubmit={loginHandler}>
-                <label htmlFor="username">Username:</label>
-                <input type="email" id="username" name="username" onChange={usernameHandler} />
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" onChange={passwordHandler} />
-                <button type="submit">Login</button>
-            </form>
-
-            <button onClick={SignUpHandler}>SignUp</button>
+  return (
+    <div className="background">
+      <div className="login-container">
+        <div className="left-login-image"></div>
+        <div className="right-login">
+          <img
+            src={qualitas_logo}
+            alt="Qualitas logo"
+            className="qualitas-logo"
+          />
+          <h1>Inicio de Sesión</h1>
+          <form onSubmit={loginHandler}>
+            <div>
+              <FaRegUser className="login-icon" />
+              <div className="input-container">
+                <input
+                  type="email"
+                  id="username"
+                  name="username"
+                  onChange={usernameHandler}
+                  required
+                />
+                <label
+                  htmlFor="username"
+                  className={username.trim() === "" ? "" : "mantain-position"}
+                >
+                  Username:
+                </label>
+              </div>
+            </div>
+            <div>
+              <MdOutlinePassword className="login-icon" />
+              <div className="input-container">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={passwordHandler}
+                  required
+                />
+                <label
+                  htmlFor="password"
+                  className={password.trim() === "" ? "" : "mantain-position"}
+                >
+                  Password:
+                </label>
+              </div>
+            </div>
+            <button type="submit">Login</button>
+          </form>
+          <p>
+            ¿Olvidaste tu contraseña?
+            <br />
+            Haz click <a href="#">aquí</a>.
+          </p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
