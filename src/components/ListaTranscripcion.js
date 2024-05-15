0beb951126503217ebb701a298990882c38ce2e4
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-05-15 10:58:48 -0600

    Intento fallido para mostrar la transcripcion a través del backend

--- src/components/ListaTranscripcion.js
+++ src/components/ListaTranscripcion.js
@@ -5,37 +5,69 @@ import Transcripcion from './Transcripcion';
 
 
 
-const ListaTranscripcion = () => {
+const ListaTranscripcion = ({clientContactId}) => {
   // Datos temporales
 
   const [arrTranscripcion, setTranscripcion] = useState([]);
-  const [url] = useState("http://localhost:8080/agente/consultaTranscripcion2");
+  const [url] = useState("https://marmoset-rested-moccasin.ngrok-free.app/agente/consultaTranscripcion2");
 
   const descargar = useCallback(async () => {
-    try {
-      const response = await fetch(url);
-      const data  = await response.json();
-      const arrNuevo = data[0].Segments.map((segment) => {
-        const transcripcion = {
-          id: uuidv4(),
-          descripcion: segment.Transcript.Content,
-          sentiment: segment.Transcript.Sentiment,
-          rol: segment.Transcript.ParticipantRole,
-        };
-        return transcripcion;
-      });
-     
-      setTranscripcion(arrNuevo);
-    } catch (error) {
-      console.error('Error al descargar los datos:', error);
-    }
-  }, [url]);
+    // try {
+    //   if (clientContactId === null) {
+    //     return;
+    //   }
+    //   fetch(`${url}?contactId=${clientContactId}`, {
+    //     method: 'GET',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //     }
+    //   }).then((response) => {
+    //     if (response) {
+    //       console.log('Respuesta:', response);
+    //       const data = response.json();
+    //     }
+    //   }).then((data) => {
+    //     const arrNuevo = data[0].Segments.map((segment) => {
+    //       const transcripcion = {
+    //         id: uuidv4(),
+    //         descripcion: segment.Transcript.Content,
+    //         sentiment: segment.Transcript.Sentiment,
+    //         rol: segment.Transcript.ParticipantRole,
+    //       }
+    //       return transcripcion;
+    //     });
+    //   }).then((arrNuevo) => {
+    //     setTranscripcion(arrNuevo);
+    //   }).catch((error) => {
+    //     console.error('Error al descargar los datos:', error);
+    //   });
+    // } catch (error) {
+    //   console.error('Error al descargar los datos:', error);
+    // }
+
+    // try {
+    //   if (clientContactId === null) {
+    //     return;
+    //   }
+    //   fetch("https://marmoset-rested-moccasin.ngrok-free.app/messages/prueba", {
+    //     method: 'GET',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //     }
+    //   }).then((response) => {
+    //     console.log('Respuesta:', response);
+    //   }).catch((error) => {
+    //     alert('Error al descargar los datos:', error);
+    //   });
+    // } catch (error) {
+    //   console.error('Error al descargar los datos:', error);
+    // }
+  }, [url, clientContactId]);
 
   useEffect(() => {
-    
     const interval = setInterval(descargar, 3000); // Descargar cada 5 segundos
     return () => clearInterval(interval); // Limpiar intervalo al desmontar el componente
-  }, [descargar]);
+  });
 
 
     return(
