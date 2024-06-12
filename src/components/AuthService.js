b3dd702d22b4fd8b580d3edc51a169d57ea34523
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-06-12 07:06:03 -0600

    Formato de archivo authService actualizado

--- src/components/AuthService.js
+++ src/components/AuthService.js
@@ -1,34 +1,45 @@
-import { CognitoIdentityProviderClient, ConfirmForgotPasswordCommand, ForgotPasswordCommand, InitiateAuthCommand } from "@aws-sdk/client-cognito-identity-provider";
+import {
+  CognitoIdentityProviderClient,
+  ConfirmForgotPasswordCommand,
+  ForgotPasswordCommand,
+  InitiateAuthCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 import config from "../config.json";
 
 export const cognitoClient = new CognitoIdentityProviderClient({
-    region: config.region,
+  region: config.region,
 });
 
 export const signIn = async (username, password) => {
-    const params = {
-      AuthFlow: "USER_PASSWORD_AUTH",
-      ClientId: config.clientId,
-      AuthParameters: {
-        USERNAME: username,
-        PASSWORD: password,
-      },
-    };
-    try {
-      const command = new InitiateAuthCommand(params);
-      const { AuthenticationResult } = await cognitoClient.send(command);
-      console.log("AuthenticationResult: ", AuthenticationResult);
-      if (AuthenticationResult) {
-        sessionStorage.setItem("idToken", AuthenticationResult.IdToken || '');
-        sessionStorage.setItem("accessToken", AuthenticationResult.AccessToken || '');
-        sessionStorage.setItem("refreshToken", AuthenticationResult.RefreshToken || '');
-        return AuthenticationResult;
-      }
-    } catch (error) {
-      console.error("Error signing in: ", error);
-      throw error;
-    }
+  const params = {
+    AuthFlow: "USER_PASSWORD_AUTH",
+    ClientId: config.clientId,
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
+    },
   };
+  try {
+    const command = new InitiateAuthCommand(params);
+    const { AuthenticationResult } = await cognitoClient.send(command);
+    console.log("AuthenticationResult: ", AuthenticationResult);
+    if (AuthenticationResult) {
+      sessionStorage.setItem("idToken", AuthenticationResult.IdToken || "");
+      sessionStorage.setItem(
+        "accessToken",
+        AuthenticationResult.AccessToken || ""
+      );
+      sessionStorage.setItem(
+        "refreshToken",
+        AuthenticationResult.RefreshToken || ""
+      );
+      return AuthenticationResult;
+    }
+  } catch (error) {
+    console.error("Error signing in: ", error);
+    throw error;
+  }
+};
 
 export const forgotPassw = async (username) => {
   try {
@@ -44,7 +55,7 @@ export const forgotPassw = async (username) => {
     console.error("Error forgot password: ", error);
     throw error;
   }
-}
+};
 
 export const confirmForgotPassw = async (username, code, newPassword) => {
   try {
@@ -62,4 +73,4 @@ export const confirmForgotPassw = async (username, code, newPassword) => {
     console.error("Error confirm forgot password: ", error);
     throw error;
   }
-}
+};
